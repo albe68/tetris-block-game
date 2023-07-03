da0d6f8f4461f2ac8345573b04e721964a91a6ae
albe68 <albertpaulus836@gmail.com>
2023-07-03 15:32:08 +0530

    first commit

---
+++ tetris-block/src/Components/Cell.tsx
@@ -0,0 +1,12 @@
+import { CellOptions } from "../types";
+
+interface Props{    
+    type: CellOptions;
+}
+
+function Cell ({ type }:Props){
+    return <div className={`cell ${type}`}></div>;
+
+}
+
+export default Cell;
